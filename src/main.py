2b6af21214c5b30637854be5a337fc1479ec99ad
sky-cake <sky-cake>
2024-01-19 01:14:12 -0500

    update: production configs

--- src/main.py
+++ src/main.py
@@ -37,4 +37,5 @@ def create_app():
 
 app = create_app()
 
-app.run(CONSTS.site_host, port=CONSTS.site_port, debug=CONSTS.TESTING, certfile=CONSTS.cert_file, keyfile=CONSTS.key_file)
+if __name__=='__main__':
+    app.run(CONSTS.site_host, port=CONSTS.site_port, debug=CONSTS.TESTING, certfile=CONSTS.cert_file, keyfile=CONSTS.key_file)
