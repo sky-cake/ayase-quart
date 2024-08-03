7501ef2a0a98653360b3f3cb95bdd8fd0932d046
fireballz22 <fireballz@soa79.ca>
2024-08-03 14:58:38 -0400

    Hook up index create and update commands to cli (#17)
    Co-authored-by: fireballz22 <>

--- src/search/__main__.py
+++ src/search/__main__.py
@@ -2,17 +2,37 @@ import asyncio
 import sys
 
 from .loader import main as load
+from .providers import get_search_provider
 
 help_text = """
 usage: python -m search COMMAND [args]
 commands:
+	create board1 [board2 [board3 ...]]
+		create indexes for the search provider
 	load board1 [board2 [board3 ...]]
+		index boards in the search provider indexes
+	delete
+		delete the indexes of the search provider
 """
 
 
 def print_help():
     print(help_text)
 
+
+async def create_index():
+    if input('Create index? (y/n): ').strip().lower() != 'y': return
+    sp = get_search_provider()
+    await sp.init_indexes()
+    await sp.close()
+    print('Indexes created')
+
+async def delete_index():
+    if input('Wipe index? (y/n): ').strip().lower() != 'y': return
+    sp = get_search_provider()
+    await sp.posts_wipe()
+    await sp.close()
+    print('Index data wiped')
 
 def main(args):
     if not args:
@@ -21,6 +41,10 @@ def main(args):
     match args[0]:
         case 'load':
             asyncio.run(load(args[1:]))
+        case 'create':
+            asyncio.run(create_index())
+        case 'delete':
+            asyncio.run(delete_index())
         case _:
             print_help()
             sys.exit()
