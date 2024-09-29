1c977cda826a3422786a59a4e21ac8d07dbb5b27
fireballz22 <>
2024-09-29 15:50:08 -0400

    Eliminate non current app db instances (we never used them)

--- src/db/db_sqlite.py
+++ src/db/db_sqlite.py
@@ -10,15 +10,14 @@ from .db_interface import DatabaseInterface, row_factory
 
 class SQLiteDatabaseAppContext(DatabaseInterface):
     async def connect(self):
-        print('Creating database pool, started.')
         current_app.pool = await aiosqlite.connect(CONSTS.db_path)
         current_app.pool.row_factory = row_factory
-        print('Creating database pool, completed.')
+        print('Sqlite pool open')
 
     async def disconnect(self):
         await current_app.pool.close()
 
-    async def query_execute(self, sql, params=None, fetchone=False, commit=False):
+    async def query_execute(self, sql: str, params=None, fetchone=False, commit=False):
         sql = patch_query(sql)
 
         if CONSTS.sql_echo:
@@ -37,48 +36,18 @@ class SQLiteDatabaseAppContext(DatabaseInterface):
             return await cursor.fetchall()
 
 
-class SQLiteDatabase(DatabaseInterface):
-    def __init__(self):
-        self.pool = None
-
-    async def connect(self):
-        print('Creating database pool, started.')
-        self.pool = await aiosqlite.connect(CONSTS.db_path)
-        self.pool.row_factory = row_factory
-        print('Creating database pool, completed.')
-
-    async def disconnect(self):
-        await self.pool.close()
-
-    async def query_execute(self, sql, params=None, fetchone=False, commit=False):
-        sql = patch_query(sql)
-
-        if CONSTS.SQLALCHEMY_ECHO:
-            print(sql)
-            print(params)
-
-        async with self.pool.execute(sql, params) as cursor:
-
-            if commit:
-                self.pool.commit()
-                return
-
-            if fetchone:
-                return await cursor.fetchone()
-
-            return await cursor.fetchall()
-
-
 # Functions below are for doing Tuple queries, which are faster fetching Dict results
 
-
-async def _get_pool():
+async def _get_pool(store=True):
     # we apply an attribute on this function to avoid polluting the module's namespace
-    if not hasattr(_get_pool, 'pool'):
-        _get_pool.pool = await aiosqlite.connect(
+    if not hasattr(_get_pool, 'pool') or not store:
+        pool = await aiosqlite.connect(
             CONSTS.db_path,
             autocommit=True,
         )
+        if not store:
+            return pool
+        _get_pool.pool = pool
     return _get_pool.pool
 
 
@@ -109,5 +78,4 @@ def patch_query(query: str) -> str:
     return re_mysql_bind_to_sqlite_bind.sub(r':\1', query).replace('`', '').replace('%s', '?').replace("strftime('?', ", "strftime('%s', ")
 
 
-Database = SQLiteDatabase
 DatabaseAppContext = SQLiteDatabaseAppContext
