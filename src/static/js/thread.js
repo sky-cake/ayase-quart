2a02852c3c3e0d200c4ab17cc92b0ff49b7ad64e
sky-cake <sky-cake>
2024-11-02 12:29:43 -0400

    fix: quotelink hover

--- src/static/js/thread.js
+++ src/static/js/thread.js
@@ -31,50 +31,43 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-// quotelink hovering
 document.addEventListener("DOMContentLoaded", () => {
-
-    const op = document.querySelector('.op')
+    const op = document.querySelector('.op');
     const op_num = op ? op.getAttribute('id').split('p')[1] : null;
-    
     const quotelinks = document.querySelectorAll("a.quotelink");
 
     quotelinks.forEach((quotelink) => {
-
         const post_num = quotelink.getAttribute("href").split("#p")[1];
         const board_shortname = quotelink.getAttribute('data-board_shortname');
 
         quotelink.addEventListener("mouseover", (e) => {
             const id_post_num = "#p" + post_num;
-            var target_post = document.querySelector(id_post_num);
+            let target_post = document.querySelector(id_post_num);
 
-            if (target_post == null)
-            {
+            quotelink_preview_hide();
+
+            if (target_post == null) {
                 fetch(`/${board_shortname}/post/${post_num}`)
-                .then(response => {
-                    quotelink_preview_hide();
-                    return response.json();
-                })
-                .catch(error => {
-                    var target_post = document.createElement("div");
-                    target_post.innerHTML = get_quotelink_preview_default_string();
-                    quotelink_preview_show(target_post, quotelink);
-                })
-                .then(data => {
-                    if (data){
-                        var target_post = document.createElement("div");
-                        target_post.innerHTML = data.html_content;
+                    .then(response => {
+                        return response.ok ? response.json() : Promise.reject();
+                    })
+                    .then(data => {
+                        let previewContent = data ? data.html_content : get_quotelink_preview_default_string();
+                        target_post = document.createElement("div");
+                        target_post.innerHTML = previewContent;
                         quotelink_preview_show(target_post, quotelink);
-                    }
-                });
-            }
-            else
-            {
+                    })
+                    .catch(() => {
+                        let default_preview = document.createElement("div");
+                        default_preview.innerHTML = get_quotelink_preview_default_string();
+                        quotelink_preview_show(default_preview, quotelink);
+                    });
+            } else {
                 quotelink_preview_show(target_post, quotelink);
             }
         });
 
-        quotelink.addEventListener("mouseout", () => {
+        quotelink.addEventListener("mouseleave", () => {
             quotelink_preview_hide();
         });
 
@@ -94,7 +87,6 @@ function get_quotelink_preview_default_string(){
                     <span class="name">Admin</span>
                 </span>
             </div>
-            
             <blockquote class="postMessage">Could not locate post</blockquote>
         </div>
     </div>`;
@@ -107,50 +99,38 @@ function quotelink_preview_hide(){
     });
 }
 
-
-function quotelink_preview_show(target_post, quotelink){
-    var preview = target_post.cloneNode(true);
-    preview.id = "quote-preview"; // set the id to delete after mouse hover
+function quotelink_preview_show(target_post, quotelink) {
+    let preview = target_post.cloneNode(true);
+    preview.id = "quote-preview";
 
     document.body.appendChild(preview);
 
-    var ql = quotelink.getBoundingClientRect();
+    const ql = quotelink.getBoundingClientRect();
+    const prev = preview.getBoundingClientRect();
+    const scroll_y = document.documentElement.scrollTop;
 
-    var prev = preview.getBoundingClientRect();
-
-    var scroll_y = document.documentElement.scrollTop;
-
-    // default preview coords
-    var top = ql.bottom + scroll_y - prev.height / 2;
-    var left = ql.right + 5;
-    var right = prev.right;
+    // Default preview coordinates
+    let top = ql.bottom + scroll_y - prev.height / 2;
+    let left = ql.right + 5;
 
     // don't clip top
     if (top < scroll_y) {
-        var top = scroll_y;
+        top = scroll_y;
     }
 
-    // flip side that preview quotelink is on if not much space on right side of it
-    if (ql.right > window.outerWidth / 1.5) {
+    // Flip side if there's not enough space on the right
+    if (ql.right > window.innerWidth / 1.5) {
         left = ql.left - prev.width - 5;
     }
 
-    // don't clip right
-    if (right > window.outerWidth) {
-        preview.style.width = window.outerWidth - right - 10 + "px";
+    // Adjust width to prevent clipping on the right
+    if (left + prev.width > window.innerWidth) {
+        preview.style.width = `${window.innerWidth - left - 10}px`;
     }
 
-    // don't clip left
-    if (left < 0) {
-        left = 0;
-        if (right > ql.left) {
-            preview.style.width = ql.left - 10 + "px";
-        }
-    }
-
-    preview.style.top = top + "px";
-    preview.style.left = left + "px";
-    
+    // Set position
+    preview.style.top = `${top}px`;
+    preview.style.left = `${left}px`;
     preview.style.border = "1px solid white";
-    preview.style.backgroundColor = "#282a2e"; // TODO: support other themes
+    preview.style.backgroundColor = "#282a2e";
 }
