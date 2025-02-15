64c8b57352c0bdef0dcb2835aadaf1104a5de36c
sky-cake <sky-cake>
2025-02-15 14:53:15 -0500

    revert js

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,18 +1,18 @@
 function pointToOtherMediaOnError(e) {
   var ext = e.getAttribute("data-ext");
-  if (ext === "webm" || ext === "mp4") {
-    return;
+  if (ext === "webm") {
+      return;
   }
 
   const current_src = e.getAttribute('src');
   const thumb_src = e.getAttribute('data-thumb_src');
   const full_media_src = e.getAttribute('data-full_media_src');
 
-  if (current_src === thumb_src && full_media_src) {
-    e.src = full_media_src;
+  if (current_src === thumb_src && full_media_src){
+      e.src = full_media_src;
   }
   else {
-    e.src = thumb_src;
+      e.src = thumb_src;
   }
 
   e.onerror = null;
@@ -20,7 +20,7 @@ function pointToOtherMediaOnError(e) {
 
 
 function removeClonedImages() {
-  document.querySelectorAll('#img_cloned').forEach(i => { i.remove() });
+  document.querySelectorAll('#img_cloned').forEach(i => {i.remove()});
 }
 
 
@@ -28,61 +28,61 @@ function expandMedia(e) {
   var ext = e.getAttribute("data-ext");
 
   removeClonedImages();
+  
+  if (ext === "webm") {
+      var video = document.createElement("video");
+      video.setAttribute("controls", true);
+      video.setAttribute("autoplay", true);
+      video.setAttribute("style", "max-height: 90vh; max-width: 90vw;");
 
-  if (ext === "webm" || ext === "mp4") {
-    var video = document.createElement("video");
-    video.setAttribute("controls", true);
-    video.setAttribute("autoplay", true);
-    video.setAttribute("style", "max-height: 90vh; max-width: 90vw;");
+      var source = document.createElement("source");
+      source.setAttribute("src", e.getAttribute("data-full_media_src"));
+      source.setAttribute("type", "video/webm");
+      
+      video.appendChild(source);
 
-    var source = document.createElement("source");
-    source.setAttribute("src", e.getAttribute("data-full_media_src"));
-    ext === "webm" ? source.setAttribute("type", "video/webm") : source.setAttribute("type", "video/mp4");
-
-    video.appendChild(source);
-
-    e.parentNode.replaceChild(video, e);
-    return;
+      e.parentNode.replaceChild(video, e);
+      return;
   }
 
   var img = e.cloneNode();
-  if (e.getAttribute("data-expanded") === "false") {
-    img.setAttribute("data-expanded", "true");
+  if (e.getAttribute("data-expanded") === "false"){
+      img.setAttribute("data-expanded", "true");
 
-    img.setAttribute("src", e.getAttribute("data-full_media_src"));
-
-    img.setAttribute("style", "max-height: 90vh; max-width: 90vw;");
-    img.setAttribute("data-width", e.getAttribute("width"));
-    img.setAttribute("data-height", e.getAttribute("height"));
-    img.removeAttribute("width", null);
-    img.removeAttribute("height", null);
+      img.setAttribute("src", e.getAttribute("data-full_media_src"));
+      
+      img.setAttribute("style", "max-height: 90vh; max-width: 90vw;");
+      img.setAttribute("data-width", e.getAttribute("width"));
+      img.setAttribute("data-height", e.getAttribute("height"));
+      img.removeAttribute("width", null);
+      img.removeAttribute("height", null);
   }
-  else {
-    img.setAttribute("data-expanded", "false");
+  else{
+      img.setAttribute("data-expanded", "false");
+      
+      img.removeAttribute("style");
 
-    img.removeAttribute("style");
+      img.setAttribute("style", "max-height: 90vh; max-width: 90vw;");
+      img.setAttribute("width", e.getAttribute("data-width"));
+      img.setAttribute("height", e.getAttribute("data-height"));
 
-    img.setAttribute("style", "max-height: 90vh; max-width: 90vw;");
-    img.setAttribute("width", e.getAttribute("data-width"));
-    img.setAttribute("height", e.getAttribute("data-height"));
+      img.setAttribute("src", e.getAttribute("data-thumb_src"));
 
-    img.setAttribute("src", e.getAttribute("data-thumb_src"));
+
   }
   e.parentNode.replaceChild(img, e);
-
-  document.querySelectorAll('img').forEach(img => {
-    img.addEventListener('mouseover', preview_on_hover);
-  });
 }
 
+
+
 function checkAllBoards() {
-  document.querySelectorAll('#boards input[type="checkbox"]').forEach(function (checkbox) {
-    checkbox.checked = true;
+  document.querySelectorAll('#boards input[type="checkbox"]').forEach(function(checkbox) {
+      checkbox.checked = true;
   });
 }
 
 function uncheckAllBoards() {
-  document.querySelectorAll('#boards input[type="checkbox"]').forEach(function (checkbox) {
-    checkbox.checked = false;
+  document.querySelectorAll('#boards input[type="checkbox"]').forEach(function(checkbox) {
+      checkbox.checked = false;
   });
 }
--- src/static/js/thread.js
+++ src/static/js/thread.js
@@ -1,153 +1,155 @@
+// image hover display
 document.addEventListener("DOMContentLoaded", () => {
 
-  function preview_on_hover() {
-    const img_cloned = document.createElement('img');
-    img_cloned.id = 'img_cloned';
-
-    const full_media_src = this.getAttribute('data-full_media_src');
-    const thumb_src = this.getAttribute('data-thumb_src');
-
-    var src = '';
-    if (full_media_src) {
-      src = full_media_src;
-    }
-    else if (thumb_src) {
-      src = thumb_src;
-    }
-
-    if (src != '') {
-      img_cloned.src = full_media_src;
-      img_cloned.classList.add('hover_image');
-
-      img_cloned.classList.add('hover_image');
-      document.body.appendChild(img_cloned);
-
-      img.addEventListener('mouseout', removeClonedImages);
-    }
-  }
-
   document.querySelectorAll('img').forEach(img => {
-    img.addEventListener('mouseover', preview_on_hover);
-  });
+      img.addEventListener('mouseover', function () {
+          const img_cloned = document.createElement('img');
+          img_cloned.id = 'img_cloned';
 
+          const full_media_src = this.getAttribute('data-full_media_src');
+          const thumb_src = this.getAttribute('data-thumb_src');
+
+          var src = '';
+          if (full_media_src){
+              src = full_media_src;
+          }
+          else if (thumb_src) {
+              src = thumb_src;
+          }
+
+          if (src != ''){
+              img_cloned.src = full_media_src;
+              img_cloned.classList.add('hover_image');
+
+              img_cloned.classList.add('hover_image');
+              document.body.appendChild(img_cloned);
+
+              img.addEventListener('mouseout', removeClonedImages);
+          }
+      });
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
   const op = document.querySelector('.op');
   const op_num = op ? op.getAttribute('id').split('p')[1] : null;
   const quotelinks = document.querySelectorAll("a.quotelink");
 
   quotelinks.forEach((quotelink) => {
-    const num = quotelink.getAttribute("href").split("#p")[1];
-    const board_shortname = quotelink.getAttribute('data-board_shortname');
+      const num = quotelink.getAttribute("href").split("#p")[1];
+      const board_shortname = quotelink.getAttribute('data-board_shortname');
 
-    quotelink.addEventListener("mouseover", (e) => {
-      let backlink = e.target.parentElement.parentElement.id;
-      let backlink_num = backlink ? backlink.replace(/^bl_/, '') : null;
+      quotelink.addEventListener("mouseover", (e) => {
+          let backlink = e.target.parentElement.parentElement.id;
+          let backlink_num = backlink ? backlink.replace(/^bl_/, '') : null;
+          
+          const id_post_num = "#p" + num;
+          let target_post = document.querySelector(id_post_num);
 
-      const id_post_num = "#p" + num;
-      let target_post = document.querySelector(id_post_num);
+          quotelink_preview_hide();
 
-      quotelink_preview_hide();
+          if (target_post == null) {
+              fetch(`/${board_shortname}/post/${num}`)
+                  .then(response => {
+                      return response.ok ? response.json() : Promise.reject();
+                  })
+                  .then(data => {
+                      let previewContent = data ? data.html_content : get_quotelink_preview_default_string();
+                      target_post = document.createElement("div");
+                      target_post.innerHTML = previewContent;
+                      quotelink_preview_show(target_post, quotelink, backlink_num);
+                  })
+                  .catch(() => {
+                      let default_preview = document.createElement("div");
+                      default_preview.innerHTML = get_quotelink_preview_default_string();
+                      quotelink_preview_show(default_preview, quotelink, backlink_num);
+                  });
+          } else {
+              quotelink_preview_show(target_post, quotelink, backlink_num);
+          }
+      });
 
-      if (target_post == null) {
-        fetch(`/${board_shortname}/post/${num}`)
-          .then(response => {
-            return response.ok ? response.json() : Promise.reject();
-          })
-          .then(data => {
-            let previewContent = data ? data.html_content : get_quotelink_preview_default_string();
-            target_post = document.createElement("div");
-            target_post.innerHTML = previewContent;
-            quotelink_preview_show(target_post, quotelink, backlink_num);
-          })
-          .catch(() => {
-            let default_preview = document.createElement("div");
-            default_preview.innerHTML = get_quotelink_preview_default_string();
-            quotelink_preview_show(default_preview, quotelink, backlink_num);
-          });
-      } else {
-        quotelink_preview_show(target_post, quotelink, backlink_num);
+      quotelink.addEventListener("mouseleave", () => {
+          quotelink_preview_hide();
+      });
+
+      // OP labels on quotelinks
+      if(num == op_num) {
+          quotelink.textContent += ' (OP)';
       }
-    });
-
-    quotelink.addEventListener("mouseleave", () => {
-      quotelink_preview_hide();
-    });
-
-    // OP labels on quotelinks
-    if (num == op_num) {
-      quotelink.textContent += ' (OP)';
-    }
   });
-
-  function get_quotelink_preview_default_string() {
-    return `<div class="postContainer replyContainer">
-            <div class="post reply">
-                <div class="postInfo desktop">
-                    <span class="nameBlock">
-                        <span class="name">Admin</span>
-                    </span>
-                </div>
-                <blockquote class="postMessage">Could not locate post</blockquote>
-            </div>
-        </div>`;
-  }
+});
 
 
-  function quotelink_preview_hide() {
-    document.querySelectorAll("#quote-preview").forEach(qp => {
+function get_quotelink_preview_default_string(){
+  return `<div class="postContainer replyContainer">
+      <div class="post reply">
+          <div class="postInfo desktop">
+              <span class="nameBlock">
+                  <span class="name">Admin</span>
+              </span>
+          </div>
+          <blockquote class="postMessage">Could not locate post</blockquote>
+      </div>
+  </div>`;
+}
+
+
+function quotelink_preview_hide(){
+  document.querySelectorAll("#quote-preview").forEach(qp => {
       qp.remove()
-    });
-  }
+  });
+}
 
 
-  function quotelink_preview_show(target_post, quotelink, backlink_num) {
-    let preview = target_post.cloneNode(true);
-    preview.id = "quote-preview";
+function quotelink_preview_show(target_post, quotelink, backlink_num) {
+  let preview = target_post.cloneNode(true);
+  preview.id = "quote-preview";
 
-    // highlight the recipient of the reply to help when there are multiple quotelinks
-    const board_shortname = quotelink.getAttribute("data-board_shortname");
-    const recipients = preview.querySelectorAll(`a.quotelink`);
-    for (const recipient of recipients) {
+  // highlight the recipient of the reply to help when there are multiple quotelinks
+  const board_shortname = quotelink.getAttribute("data-board_shortname");
+  const recipients = preview.querySelectorAll(`a.quotelink`);
+  for (const recipient of recipients) {
       const recipient_post_num = recipient.getAttribute("href").split("#p")[1];
       const recipient_board_shortname = recipient.getAttribute("data-board_shortname");
-
+  
       if (recipient_post_num === backlink_num && recipient_board_shortname === board_shortname) {
-        recipient.classList.add("hl_dark");
-        break;
+          recipient.classList.add("hl_dark");
+          break;
       }
-    }
-
-    preview.addEventListener("mouseover", (e) => { quotelink_preview_hide(); });
-
-    document.body.appendChild(preview);
-
-    const ql = quotelink.getBoundingClientRect();
-    const prev = preview.getBoundingClientRect();
-    const scroll_y = document.documentElement.scrollTop;
-
-    // Default preview coordinates
-    let top = ql.bottom + scroll_y - prev.height / 2;
-    let left = ql.right + 5;
-
-    // don't clip top
-    if (top < scroll_y) {
-      top = scroll_y;
-    }
-
-    // Flip side if there's not enough space on the right
-    if (ql.right > window.innerWidth / 1.5) {
-      left = ql.left - prev.width - 5;
-    }
-
-    // Adjust width to prevent clipping on the right
-    if (left + prev.width > window.innerWidth) {
-      preview.style.width = `${window.innerWidth - left - 20}px`;
-    }
-
-    // Set position
-    preview.style.top = `${top}px`;
-    preview.style.left = `${left}px`;
-    preview.style.border = "1px solid white";
-    preview.style.backgroundColor = "#282a2e";
   }
 
-});
+  preview.addEventListener("mouseover", (e) => {quotelink_preview_hide();});
+
+  document.body.appendChild(preview);
+
+  const ql = quotelink.getBoundingClientRect();
+  const prev = preview.getBoundingClientRect();
+  const scroll_y = document.documentElement.scrollTop;
+
+  // Default preview coordinates
+  let top = ql.bottom + scroll_y - prev.height / 2;
+  let left = ql.right + 5;
+
+  // don't clip top
+  if (top < scroll_y) {
+      top = scroll_y;
+  }
+
+  // Flip side if there's not enough space on the right
+  if (ql.right > window.innerWidth / 1.5) {
+      left = ql.left - prev.width - 5;
+  }
+
+  // Adjust width to prevent clipping on the right
+  if (left + prev.width > window.innerWidth) {
+      preview.style.width = `${window.innerWidth - left - 20}px`;
+  }
+
+  // Set position
+  preview.style.top = `${top}px`;
+  preview.style.left = `${left}px`;
+  preview.style.border = "1px solid white";
+  preview.style.backgroundColor = "#282a2e";
+}
