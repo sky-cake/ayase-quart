38c7570f9d860c2b93ac81cbaaed8baead5e88eb
fireballz22 <>
2025-06-14 18:22:37 -0400

    loader: integrate incremental into cli entrypoint

--- src/search/__main__.py
+++ src/search/__main__.py
@@ -4,7 +4,7 @@ import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-from search.loader import main as load
+from search.loader import load_full, load_incremental
 from search.providers import get_index_search_provider
 
 help_text = """
@@ -13,9 +13,10 @@ e.g. ./ayase-quart/src$ python3.13 -m search load --reset g ck biz
 commands:
     create
         create search indexes
-    load [--reset] board1 [board2 [board3 ...]]
+    load [--reset|--incr] board1 [board2 [board3 ...]]
         index boards (ensure indexes have been created)
         passing --reset causes a delete and recreate of the index before loading
+        passing --incr will only load posts that haven't been indexed yet
     delete
         delete search indexes
 """
@@ -54,13 +55,22 @@ def main(args):
                 print("Missing boards.")
                 print_help()
                 sys.exit()
+
+            incremental = False
+            reset = False
             if args[0] == '--reset':
                 reset = True
                 boards = args[1:]
+            elif args[0] == '--incr':
+                incremental = True
+                boards = args[1:]
             else:
-                reset = False
                 boards = args
-            asyncio.run(load(boards, reset))
+
+            if incremental:
+                asyncio.run(load_incremental(boards))
+            else:
+                asyncio.run(load_full(boards, reset))
         case 'create':
             asyncio.run(create_index())
         case 'delete':
