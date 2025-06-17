99aa482279ed2d8c82c6d935038184b6f7972daf
fireballz22 <fireballz@soa79.ca>
2025-06-17 13:14:49 -0400

    Side tables round 2 (#76)
    * sidetables: backuptable command backs up current side tables
    
    * sidetables: extract indexes from _users table
    
    * sidetables: populate multiple boards + refactor
    
    * sidetables: init sidetables commands enum
    
    ---------
    
    Co-authored-by: fireballz22 <>

---
+++ src/sidetables/__init__.py
@@ -0,0 +1,8 @@
+from enum import StrEnum
+
+class SideTableCmd(StrEnum):
+    table_create = 'createtable'
+    table_drop = 'droptable'
+    table_backup = 'backuptable'
+    index_add = 'addindex'
+    index_drop = 'dropindex'
--- src/sidetables/tables_indexes.py
+++ src/sidetables/tables_indexes.py
@@ -5,11 +5,13 @@ from configs import db_conf
 from db import db_q
 
 db_type: DbType = db_conf['db_type']
+backup_suffix = 'bak'
 
 @dataclass
 class SidetableTemplate:
     create_table: str = ''
     drop_table: str = ''
+    backup_table: str = ''
     add_index: str = ''
     drop_index: str = ''
 
@@ -19,6 +21,8 @@ class SidetableTemplate:
                 return self.create_table
             case 'droptable':
                 return self.drop_table
+            case 'backuptable':
+                return self.backup_table
             case 'dropindex':
                 return self.drop_index
             case 'addindex':
@@ -64,10 +68,7 @@ CREATE TABLE IF NOT EXISTS `%%BOARD%%_users` (
     `firstseen` int(11) NOT NULL,
     `postcount` int(11) NOT NULL,
 
-    PRIMARY KEY (`user_id`),
-    UNIQUE name_trip_index (`name`, `trip`),
-    INDEX firstseen_index (`firstseen`),
-    INDEX postcount_index (`postcount`)
+    PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `%%BOARD%%_daily` (
@@ -79,16 +80,24 @@ CREATE TABLE IF NOT EXISTS `%%BOARD%%_daily` (
     `trips` int(10) unsigned NOT NULL,
     `names` int(10) unsigned NOT NULL,
 
-  PRIMARY KEY (`day`)
+    PRIMARY KEY (`day`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 """
 
 drop_table_mysql = """
-DROP TABLE IF EXISTS `%%BOARD%%_daily`;
-DROP TABLE IF EXISTS `%%BOARD%%_deleted`;
-DROP TABLE IF EXISTS `%%BOARD%%_threads`;
-DROP TABLE IF EXISTS `%%BOARD%%_images`;
-DROP TABLE IF EXISTS `%%BOARD%%_users`;
+drop table if exists `%%BOARD%%_daily`;
+drop table if exists `%%BOARD%%_deleted`;
+drop table if exists `%%BOARD%%_threads`;
+drop table if exists `%%BOARD%%_images`;
+drop table if exists `%%BOARD%%_users`;
+"""
+
+backup_table_mysql = f"""
+rename table `%%BOARD%%_daily` to `%%BOARD%%_daily_{backup_suffix}`;
+rename table `%%BOARD%%_deleted` to `%%BOARD%%_deleted_{backup_suffix}`;
+rename table `%%BOARD%%_images` to `%%BOARD%%_images_{backup_suffix}`;
+rename table `%%BOARD%%_threads` to `%%BOARD%%_threads_{backup_suffix}`;
+rename table `%%BOARD%%_users` to `%%BOARD%%_users_{backup_suffix}`;
 """
 
 drop_index_mysql = """
@@ -102,6 +111,10 @@ drop index `locked_index` on `%%BOARD%%_threads`;
 drop index `media_hash_index` on `%%BOARD%%_images`;
 drop index `total_index` on `%%BOARD%%_images`;
 drop index `banned_index` on `%%BOARD%%_images`;
+
+drop index `name_trip_index` on `%%BOARD%%_users`;
+drop index `firstseen_index` on `%%BOARD%%_users`;
+drop index `postcount_index` on `%%BOARD%%_users`;
 """
 
 add_index_mysql = """
@@ -111,15 +124,21 @@ alter table `%%BOARD%%_threads` add index `time_ghost_bump_index` (`time_ghost_b
 alter table `%%BOARD%%_threads` add index `time_last_modified_index` (`time_last_modified`);
 alter table `%%BOARD%%_threads` add index `sticky_index` (`sticky`);
 alter table `%%BOARD%%_threads` add index `locked_index` (`locked`);
+
 alter table `%%BOARD%%_images` add unique index `media_hash_index` (`media_hash`);
 alter table `%%BOARD%%_images` add index `total_index` (`total`);
 alter table `%%BOARD%%_images` add index `banned_index` (`banned`);
+
+alter table `%%BOARD%%_users` add unique index name_trip_index (`name`, `trip`);
+alter table `%%BOARD%%_users` add index firstseen_index (`firstseen`);
+alter table `%%BOARD%%_users` add index postcount_index (`postcount`);
 """
 
 sidetable_templates = {
     DbType.mysql: SidetableTemplate(
         create_table=create_table_mysql,
         drop_table=drop_table_mysql,
+        backup_table=backup_table_mysql,
         drop_index=drop_index_mysql,
         add_index=add_index_mysql,
     ),
