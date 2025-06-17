5621283c9d7bdd89536ec95f21b5f256125da44b
fireballz22 <>
2025-06-17 19:28:17 -0400

    sidetables: replace command strings with enum version

--- src/sidetables/__main__.py
+++ src/sidetables/__main__.py
@@ -3,22 +3,23 @@ import sys
 
 from .tables_indexes import run_table_command
 from .populate import run_populate
+from . import SideTableCmd as Cmd
 
-help_text = '''
+help_text = f'''
 Usage: python -m sidetables COMMAND [args]
 Commands:
-    createtable board1 [board2 [board3 ...]]
+    {Cmd.table_create} board1 [board2 [board3 ...]]
         create side tables for selected boards
         only primary keys set, no other indexes
-    droptable board1 [board2 [board3 ...]]
+    {Cmd.table_drop} board1 [board2 [board3 ...]]
         drop side tables for selected boards
-    backuptable board1 [board2 [board3 ...]]
+    {Cmd.table_backup} board1 [board2 [board3 ...]]
         rename side tables for selected boards with _bak suffix
         ensure that side tables exist beforehand
-    dropindex board1 [board2 [board3 ...]]
+    {Cmd.index_drop} board1 [board2 [board3 ...]]
         drop indexes for selected boards side tables
         ensure that side tables exist beforehand
-    addindex board1 [board2 [board3 ...]]
+    {Cmd.index_add} board1 [board2 [board3 ...]]
         add indexes on side tables for selected boards 
         ensure that side tables exist beforehand
     populate board
@@ -34,7 +35,7 @@ def main(args: list[str]):
     if not args: help_exit()
     command, args = args[0], args[1:]
     match command:
-        case 'createtable' | 'droptable' | 'backuptable' | 'dropindex' | 'addindex':
+        case Cmd.table_create | Cmd.table_drop | Cmd.table_backup | Cmd.index_drop | Cmd.index_add:
             if not args: help_exit()
             boards = args
             asyncio.run(run_table_command(command, boards))
