403f9686ad9826214f7543ee602b4c52ccd2614d
sky-cake <polycrush@proton.me>
2025-06-23 20:22:30 -0400

    make boards csv for search queryparam, debug

--- src/search/handler.py
+++ src/search/handler.py
@@ -89,10 +89,9 @@ async def search_handler(search_type: SearchType, logged_in=False, is_admin=Fals
         if not search_conf['multi_board_search']:
             params['boards'] = [params['boards'][0]]
 
-    current_app.logger.error(type(params['boards']), params['boards'])
+    current_app.logger.error(f"{type(params['boards'])}  {params['boards']}")
     form: SearchForm = await search_form.create_form(meta={'csrf': False}, **params)
-
-    current_app.logger.error(type(params['boards']), params['boards'])
+    current_app.logger.error(f"{type(params['boards'])}  {params['boards']}  {form.boards.data}")
 
     is_search_request = bool(form.boards.data)
 
