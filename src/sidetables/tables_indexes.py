7890ee82837608b1be1cbfdd1a6c2cacf9a1c81c
fireballz22 <>
2025-06-25 22:11:11 -0400

    sidetables: add sqlite templates

--- src/sidetables/tables_indexes.py
+++ src/sidetables/tables_indexes.py
@@ -135,6 +135,135 @@ alter table `%%BOARD%%_users` add index firstseen_index (`firstseen`);
 alter table `%%BOARD%%_users` add index postcount_index (`postcount`);
 """
 
+create_table_sqlite = """
+create table if not exists `%%BOARD%%_deleted` (
+    doc_id integer not null primary key autoincrement,
+    media_id integer not null,
+    poster_ip text not null,
+    num integer not null,
+    subnum integer not null,
+    thread_num integer not null,
+    op integer not null,
+    timestamp integer not null,
+    timestamp_expired integer not null,
+    preview_orig text,
+    preview_w integer not null,
+    preview_h integer not null,
+    media_filename text,
+    media_w integer not null,
+    media_h integer not null,
+    media_size integer not null,
+    media_hash text,
+    media_orig text,
+    spoiler integer not null,
+    deleted integer not null,
+    capcode text not null,
+    email text,
+    name text,
+    trip text,
+    title text,
+    comment text,
+    delpass text,
+    sticky integer not null,
+    locked integer not null,
+    poster_hash text,
+    poster_country text,
+    exif text
+);
+
+create table if not exists `%%BOARD%%_threads` (
+    thread_num integer not null primary key,
+    time_op integer not null,
+    time_last integer not null,
+    time_bump integer not null,
+    time_ghost integer,
+    time_ghost_bump integer,
+    time_last_modified integer not null,
+    nreplies integer not null,
+    nimages integer not null,
+    sticky integer not null,
+    locked integer not null
+);
+
+create table if not exists `%%BOARD%%_images` (
+    media_id integer not null primary key autoincrement,
+    media_hash text not null,
+    media text,
+    preview_op text,
+    preview_reply text,
+    total integer not null,
+    banned integer not null
+);
+
+create table if not exists `%%BOARD%%_users` (
+    user_id integer not null primary key autoincrement,
+    name text not null,
+    trip text not null,
+    firstseen integer not null,
+    postcount integer not null
+);
+
+create table if not exists `%%BOARD%%_daily` (
+    day integer not null primary key,
+    posts integer not null,
+    images integer not null,
+    sage integer not null,
+    anons integer not null,
+    trips integer not null,
+    names integer not null
+);
+"""
+
+drop_table_sqlite = """
+drop table if exists `%%BOARD%%_daily`;
+drop table if exists `%%BOARD%%_deleted`;
+drop table if exists `%%BOARD%%_threads`;
+drop table if exists `%%BOARD%%_images`;
+drop table if exists `%%BOARD%%_users`;
+"""
+
+backup_table_sqlite = f"""
+alter table `%%BOARD%%_daily` rename to `%%BOARD%%_daily_{backup_suffix}`;
+alter table `%%BOARD%%_deleted` rename to `%%BOARD%%_deleted_{backup_suffix}`;
+alter table `%%BOARD%%_images` rename to `%%BOARD%%_images_{backup_suffix}`;
+alter table `%%BOARD%%_threads` rename to `%%BOARD%%_threads_{backup_suffix}`;
+alter table `%%BOARD%%_users` rename to `%%BOARD%%_users_{backup_suffix}`;
+"""
+
+drop_index_sqlite = """
+drop index if exists `%%BOARD%%_threads_time_op_index`;
+drop index if exists `%%BOARD%%_threads_time_bump_index`;
+drop index if exists `%%BOARD%%_threads_time_ghost_bump_index`;
+drop index if exists `%%BOARD%%_threads_time_last_modified_index`;
+drop index if exists `%%BOARD%%_threads_sticky_index`;
+drop index if exists `%%BOARD%%_threads_locked_index`;
+
+drop index if exists `%%BOARD%%_images_media_hash_index`;
+drop index if exists `%%BOARD%%_images_total_index`;
+drop index if exists `%%BOARD%%_images_banned_index`;
+
+drop index if exists `%%BOARD%%_users_name_trip_index`;
+drop index if exists `%%BOARD%%_users_firstseen_index`;
+drop index if exists `%%BOARD%%_users_postcount_index`;
+"""
+
+add_index_sqlite = """
+create index if not exists `%%BOARD%%_threads_time_op_index` on `%%BOARD%%_threads` (`time_op`);
+create index if not exists `%%BOARD%%_threads_time_bump_index` on `%%BOARD%%_threads` (`time_bump`);
+create index if not exists `%%BOARD%%_threads_time_ghost_bump_index` on `%%BOARD%%_threads` (`time_ghost_bump`);
+create index if not exists `%%BOARD%%_threads_time_last_modified_index` on `%%BOARD%%_threads` (`time_last_modified`);
+create index if not exists `%%BOARD%%_threads_sticky_index` on `%%BOARD%%_threads` (`sticky`);
+create index if not exists `%%BOARD%%_threads_locked_index` on `%%BOARD%%_threads` (`locked`);
+
+create unique index if not exists %%BOARD%%_images_media_hash_index on `%%BOARD%%_images` (media_id);
+create index if not exists %%BOARD%%_images_total_index on `%%BOARD%%_images` (`total`);
+create index if not exists %%BOARD%%_images_banned_index on `%%BOARD%%_images` (`banned`);
+
+create unique index if not exists %%BOARD%%_users_name_trip_index on `%%BOARD%%_users` (`name`, `trip`);
+create index if not exists %%BOARD%%_users_firstseen_index on `%%BOARD%%_users` (`firstseen`);
+create index if not exists %%BOARD%%_users_postcount_index on `%%BOARD%%_users` (`postcount`);
+"""
+
 sidetable_templates = {
     DbType.mysql: SidetableTemplate(
         create_table=create_table_mysql,
@@ -142,6 +271,13 @@ sidetable_templates = {
         backup_table=backup_table_mysql,
         drop_index=drop_index_mysql,
         add_index=add_index_mysql,
+    ),
+    DbType.sqlite: SidetableTemplate(
+        create_table=create_table_sqlite,
+        drop_table=drop_table_sqlite,
+        backup_table=backup_table_sqlite,
+        drop_index=drop_index_sqlite,
+        add_index=add_index_sqlite,
     ),
 }
 
