1e768684f54a96d6a1a560aef89aa7e5b039533d
fireballz22 <>
2025-07-02 21:41:41 -0400

    threads.js factor out events into concrete functions

--- src/static/js/thread.js
+++ src/static/js/thread.js
@@ -1,68 +1,66 @@
 // image hover display
-const media_containers = document.querySelectorAll('.media_cont');
-for (const container of media_containers) {
-    // removeClonedImages();
-    container.addEventListener('mouseover', function (e) {
-        const img = e.target;
-        if (!img.getAttribute('data-ext') || img.getAttribute('data-ext') === "webm" || img.getAttribute('data-ext') === "mp4") return;
-        if (img.getAttribute('data-expanded') === "true") return;
-        if (!(img instanceof HTMLImageElement)) return;
-        if (!img.hasAttribute('data-full_media_src') && !img.hasAttribute('data-thumb_src')) return;
-        if (document.getElementById('img_cloned')) return;
-
-        const img_cloned = document.createElement('img');
-        img_cloned.id = 'img_cloned';
-        img_cloned.classList.add('hover_image');
-        img_cloned.src = img.getAttribute('data-full_media_src') || img.getAttribute('data-thumb_src');
-        document.body.appendChild(img_cloned);
-    });
-
-    container.addEventListener('mouseout', function (e) {
-        if (e.target.tagName.toLowerCase() === 'img') {
+function media_mouseout(event) {
+    if (event.target.tagName.toLowerCase() === 'img') {
         removeClonedImages();
-        }
-    });
+    }
+}
+const video_extensionss = new Set(['mp4', 'webm']);
+function media_mouseover(event) {
+    const img = event.target;
+    const extension = img.getAttribute('data-ext');
+    if (!extension || video_extensionss.has(extension)) return;
+    if (img.getAttribute('data-expanded') === "true") return;
+    if (!(img instanceof HTMLImageElement)) return;
+    if (!img.hasAttribute('data-full_media_src') && !img.hasAttribute('data-thumb_src')) return;
+    if (document.getElementById('img_cloned')) return;
+
+    const img_cloned = document.createElement('img');
+    img_cloned.id = 'img_cloned';
+    img_cloned.classList.add('hover_image');
+    img_cloned.src = img.getAttribute('data-full_media_src') || img.getAttribute('data-thumb_src');
+    document.body.appendChild(img_cloned);
 }
 
+const media_containers = document.querySelectorAll('.media_cont');
+for (const container of media_containers) {
+    container.addEventListener('mouseover', media_mouseover);
+    container.addEventListener('mouseout', media_mouseout);
+}
 
-const quotelinks = document.querySelectorAll("a.quotelink");
-for (const quotelink of quotelinks) {
+function quoteline_mouseover(event) {
+    const quotelink = event.target;
+    const backlink = quotelink.parentElement.parentElement.id;
+
     const num = quotelink.getAttribute("href").split("#p")[1];
     const board_shortname = quotelink.getAttribute('data-board_shortname');
+    let backlink_num = backlink ? backlink.replace(/^bl_/, '') : null;
 
-    quotelink.addEventListener("mouseover", (e) => {
-        let backlink = e.target.parentElement.parentElement.id;
-        let backlink_num = backlink ? backlink.replace(/^bl_/, '') : null;
+    const id_post_num = "#p" + num;
+    let target_post = document.querySelector(id_post_num);
 
-        const id_post_num = "#p" + num;
-        let target_post = document.querySelector(id_post_num);
+    quotelink_preview_hide();
 
-        quotelink_preview_hide();
-
-        if (target_post == null) {
-            fetch(`/${board_shortname}/post/${num}`)
-                .then(response => {
-                    return response.ok ? response.json() : Promise.reject();
-                })
-                .then(data => {
-                    let previewContent = data && data.html_content ? data.html_content : get_quotelink_preview_default_string();
-                    target_post = document.createElement("div");
-                    target_post.innerHTML = previewContent;
-                    quotelink_preview_show(target_post, quotelink, backlink_num);
-                })
-                .catch(() => {
-                    let default_preview = document.createElement("div");
-                    default_preview.innerHTML = get_quotelink_preview_default_string();
-                    quotelink_preview_show(default_preview, quotelink, backlink_num);
-                });
-        } else {
+    if (target_post == null) {
+        fetch(`/${board_shortname}/post/${num}`).then(response => {
+            return response.ok ? response.json() : Promise.reject();
+        }).then(data => {
+            let previewContent = data && data.html_content ? data.html_content : get_quotelink_preview_default_string();
+            target_post = document.createElement("div");
+            target_post.innerHTML = previewContent;
             quotelink_preview_show(target_post, quotelink, backlink_num);
-        }
-    });
-
-    quotelink.addEventListener("mouseleave", () => {
-        quotelink_preview_hide();
-    });
+        }).catch(() => {
+            let default_preview = document.createElement("div");
+            default_preview.innerHTML = get_quotelink_preview_default_string();
+            quotelink_preview_show(default_preview, quotelink, backlink_num);
+        });
+    } else {
+        quotelink_preview_show(target_post, quotelink, backlink_num);
+    }
+}
+const quotelinks = document.querySelectorAll("a.quotelink");
+for (const quotelink of quotelinks) {
+    quotelink.addEventListener("mouseover", quoteline_mouseover);
+    quotelink.addEventListener("mouseleave", quotelink_preview_hide);
 }
 
 function get_quotelink_preview_default_string() {
@@ -95,7 +93,7 @@ function quotelink_preview_show(target_post, quotelink, backlink_num) {
         }
     }
 
-    preview.addEventListener("mouseover", (e) => { quotelink_preview_hide(); });
+    preview.addEventListener("mouseover", quotelink_preview_hide);
 
     document.body.appendChild(preview);
 
