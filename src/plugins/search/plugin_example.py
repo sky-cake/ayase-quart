03fba132ddb7599e058c3cfcbac2f271ac1258e9
sky-cake <polycrush@proton.me>
2025-09-08 20:48:17 -0400

    disable example search plugin

--- src/plugins/search/plugin_example.py
+++ src/plugins/search/plugin_example.py
@@ -1,7 +1,7 @@
-from plugins.search.base import SearchPlugin
-from forms import SearchForm
+# from plugins.search.base import SearchPlugin
+# from forms import SearchForm
 
 
-class SearchPluginExample(SearchPlugin):
-    async def get_boards_2_nums(self, form: SearchForm) -> dict[str, set[int]]:
-        return dict()
+# class SearchPluginExample(SearchPlugin):
+#     async def get_boards_2_nums(self, form: SearchForm) -> dict[str, set[int]]:
+#         return dict()
