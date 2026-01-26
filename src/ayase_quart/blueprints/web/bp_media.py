a397e387668be66e1c61e372866c7282f1d7b25e
sky-cake <ayasequart@proton.me>
2026-01-26 18:35:24 -0500

    no regex in serve() and serve() perf

--- src/ayase_quart/blueprints/web/bp_media.py
+++ src/ayase_quart/blueprints/web/bp_media.py
@@ -1,5 +1,4 @@
 import os
-import re
 
 from quart import Blueprint, abort
 from werkzeug.security import safe_join
@@ -10,35 +9,36 @@ from ...utils.web_helpers import send_file_no_headers
 bp = Blueprint("bp_app_media", __name__)
 
 
-def should_be_served(file_path, typ: str) -> bool:
-    re_board = '[a-zA-Z0-9]{1,5}'
-    match = re.search(f'/({re_board})/{typ}/', file_path)
-    if match:
-        return False
-    return True
-
-
 if media_conf.get('endpoint') and media_conf['serve_outside_static']:
-    @bp.route(f'/{media_conf['endpoint']}/<path:file_path>')
+    media_root = media_conf['media_root_path']
+    valid_ext = media_conf['valid_extensions']
+    boards_with_image = media_conf['boards_with_image']
+    boards_with_thumb = media_conf['boards_with_thumb']
+
+    img = '/image/'
+    thb = '/thumb/'
+
+    @bp.route(f'/{media_conf["endpoint"]}/<path:file_path>')
     async def serve(file_path: str):
-        if media_conf['boards_with_image'] and not should_be_served(file_path, 'image'):
+        # file_path = 'g/thumb/1763/69/1763698364803744s.jpg'
+        # file_path = 'g/image/1763/67/1763679461297674.jpg'
+        if boards_with_image and img not in file_path:
             abort(404)
 
-        if media_conf['boards_with_thumb'] and not should_be_served(file_path, 'thumb'):
+        if boards_with_thumb and thb not in file_path:
             abort(404)
 
-        file_path = safe_join(media_conf['media_root_path'], file_path)
-
-        if not file_path:
+        full_path = safe_join(media_root, file_path)
+        if not full_path:
             abort(404)
 
-        if not file_path.split('.')[-1].lower().endswith(media_conf['valid_extensions']):
+        if not full_path.startswith(media_root):
             abort(404)
 
-        if not file_path.startswith(media_conf['media_root_path']):
+        if not full_path.rsplit('.', 1)[-1].lower().endswith(valid_ext):
             abort(404)
 
-        if not os.path.isfile(file_path):
+        if not os.path.isfile(full_path):
             abort(404)
 
-        return await send_file_no_headers(file_path)
+        return await send_file_no_headers(full_path)
