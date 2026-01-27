5465dc6970cb74b47f94c06236a4b8cc8de943d7
sky-cake <ayasequart@proton.me>
2026-01-26 19:08:22 -0500

    serve() perf

--- src/ayase_quart/blueprints/web/bp_media.py
+++ src/ayase_quart/blueprints/web/bp_media.py
@@ -11,31 +11,34 @@ bp = Blueprint("bp_app_media", __name__)
 
 if media_conf.get('endpoint') and media_conf['serve_outside_static']:
     media_root = media_conf['media_root_path']
-    valid_ext = media_conf['valid_extensions']
-    boards_with_image = media_conf['boards_with_image']
-    boards_with_thumb = media_conf['boards_with_thumb']
+    valid_exts = set(media_conf['valid_extensions'])
+    boards_with_image = set(media_conf['boards_with_image'])
+    boards_with_thumb = set(media_conf['boards_with_thumb'])
 
-    img = '/image/'
-    thb = '/thumb/'
 
-    @bp.route(f'/{media_conf["endpoint"]}/<path:file_path>')
-    async def serve(file_path: str):
+    @bp.route(f'/{media_conf["endpoint"]}/<path:suffix_path>')
+    async def serve(suffix_path: str):
         # file_path = 'g/thumb/1763/69/1763698364803744s.jpg'
         # file_path = 'g/image/1763/67/1763679461297674.jpg'
-        if boards_with_image and img not in file_path:
+
+        board = suffix_path.split('/', maxsplit=1)[0]
+        if not board:
             abort(404)
 
-        if boards_with_thumb and thb not in file_path:
+        if suffix_path.startswith(f'{board}/image/') and board not in boards_with_image:
+            abort(404)
+        elif suffix_path.startswith(f'{board}/thumb/') and board not in boards_with_thumb:
             abort(404)
 
-        full_path = safe_join(media_root, file_path)
+        full_path = safe_join(media_root, suffix_path)
         if not full_path:
             abort(404)
 
-        if not full_path.startswith(media_root):
-            abort(404)
+        # faith in safe_join
+        # if not full_path.startswith(media_root):
+        #     abort(404)
 
-        if not full_path.rsplit('.', 1)[-1].lower().endswith(valid_ext):
+        if full_path.rsplit('.', 1)[-1].lower() not in valid_exts:
             abort(404)
 
         if not os.path.isfile(full_path):
