51ba53f71c079e4054d5208908ca10d45e6798be
sky-cake <ayasequart@proton.me>
2026-04-20 23:10:30 -0400

    remove data scripts

--- db_scripts/drop_side_tables.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'DROP TABLE IF EXISTS "' || name || '";'
-FROM sqlite_master
-WHERE type = 'table'
-  AND (name LIKE '%_images' OR name LIKE '%_threads');
-
--- run the resulting DROP statements
--- then run VACUUM; if you want
