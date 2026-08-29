69112fa0b313de6366599aa35a8890216747b57b
sky-cake <>
2026-08-29 00:57:18 -0400

    icon > # for mobile visit thread link

--- src/ayase_quart/static/js/thread.js
+++ src/ayase_quart/static/js/thread.js
@@ -104,7 +104,7 @@ function setup_quotelink_events() {
     if (is_mobile) {
         for (const quotelink of quotelinks) {
             const preview = quotelink.cloneNode();
-            preview.textContent = ' #';
+            preview.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="padding-top:1px;margin-left:1px;"><path d="m10 16 4-4-4-4"/><path d="M3 12h11"/><path d="M3 8V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-3"/></svg>`;
 
             quotelink.removeEventListener("click", remove_link);
             quotelink.addEventListener("click", remove_link);
